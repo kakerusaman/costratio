75e0b83e8ff1ad6c228920b3e91c53f51d50bcd0
kakerusaman <kakerusaman@gmail.com>
2025-07-25 22:36:19 +0900

    frontつか

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/costratio/', // GitHub Pagesのリポジトリ名を指定h
+  base: '/costratio/frontend', // GitHub Pagesのリポジトリ名を指定h
 });
